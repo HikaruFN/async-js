789d32d5933079381997021c513ef957e7196405
Francesco Nisi <48261647+HikaruFN@users.noreply.github.com>
2024-06-18 23:07:48 +0200

    Promises with Get Solution

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+const { log } = require('console');
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile('dogs.txt', 'utf8', (err, data) => {
+// Callback Hell Soultion
+/*fs.readFile('dogs.txt', 'utf8', (err, data) => {
     console.log('Breeds: ', data);
 
     superagent.get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, response) => {
+        if(err) return console.log(err.message);
         console.log(response.body.message);
-    })
-})
 
+        fs.writeFile('dog-image.txt', response.body.message, (err) => {
+            console.log('Random dog image saved to file');
+
+        })
+    })
+})*/
+
+// Promises with Get Solution
+/*fs.readFile('dogs.txt', 'utf8', (err, data)=>{
+    console.log('Breeds: ', data);
+
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`) //Il metodo get presuppone uno promise sui dati richiesti
+        .then(res=>{
+            console.log(res.body.message);
+        }) //il metodo then si attiva all'arrivo dei dati
+})*/
 
 
 
